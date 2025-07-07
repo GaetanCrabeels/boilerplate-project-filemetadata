d6395219b1d4c6017f39953539815b51baee52b0
gaetancrabeels@hotmail.com <Crabeels.gaetan@outlook.be>
2025-07-07 16:46:45 +0200

    Using express-fileupload to get the name/type/size of a file

--- index.js
+++ index.js
@@ -1,18 +1,34 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
-
 var app = express();
-
+const bodyParser = require('body-parser');
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
-
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+const fileUpload = require('express-fileupload');
+app.use(fileUpload());
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+/* Submit Form */
+app.post('/api/fileanalyse', function (req, res) {
+  const file = req.files ? req.files.upfile : null;
+  if (!file) {
+    return res.status(400).json({ error: 'No file uploaded' });
+  }
+  const fileName = file.name;
+  const fileType = file.mimetype;
+  const fileSize = file.size;
+  res.json({
+    name: fileName,
+    type: fileType,
+    size: fileSize
+  });
 
-
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
